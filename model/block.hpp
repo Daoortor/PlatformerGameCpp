221cefebeac9fada646808d759246d4e223c6a8f
timustinov <112581569+timustinov@users.noreply.github.com>
2023-03-10 23:10:51 +0300

    Update block.hpp

--- model/block.hpp
+++ model/block.hpp
@@ -1,12 +1,21 @@
 #ifndef PLATFORMERGAMECPP_BLOCK_HPP_
 #define PLATFORMERGAMECPP_BLOCK_HPP_
 
+#include <iostream>
+
 namespace Platformer {
 class Block {
+    std::string name_;
     bool isSolid_;
     bool isHangableOn_;
 
 public:
+    Block() = default;
+
+    [[nodiscard]] std::string name() {
+        return name_;
+    }
+
     [[nodiscard]] bool isSolid() const {
         return isSolid_;
     }
@@ -14,7 +23,39 @@ public:
     [[nodiscard]] bool isHangableOn() const {
         return isHangableOn_;
     }
+
+    void operator+=(Block T);
 };
+
+class Stone : public Block {
+    const std::string name_ = "stone";
+    const bool isSolid_ = true;
+    const bool isHangableOn_ = false;
+
+public:
+    Stone() = default;
+};
+
+class Air : public Block {
+    const std::string name_ = "air";
+    const bool isSolid_ = false;
+    const bool isHangableOn_ = false;
+
+public:
+    Air() = default;
+};
+
+
+class Ladder : public Block {
+    const std::string name_ = "ladder";
+    const bool isSolid_ = false;
+    const bool isHangableOn_ = true;
+
+public:
+    Ladder() = default;
+};
+
+
 }  // namespace Platformer
 
 #endif  // PLATFORMERGAMECPP_BLOCK_HPP_
