8437514755c6431b6a637d6437d999741ba741a6
timustinov <112581569+timustinov@users.noreply.github.com>
2023-03-11 09:43:48 +0300

    Переделал под полиморфизм.

--- model/block.hpp
+++ model/block.hpp
@@ -1,60 +1,96 @@
 #ifndef PLATFORMERGAMECPP_BLOCK_HPP_
 #define PLATFORMERGAMECPP_BLOCK_HPP_
 
-#include <iostream>
-
 namespace Platformer {
+// Полиморфизм работает с std::unique_ptr.
+// Можно переделать без использования наследования.
 class Block {
-    std::string name_;
-    bool isSolid_;
-    bool isHangableOn_;
+protected:
+    const std::string name_;
+    const bool isSolid_ = false;
+    const bool isHangableOn_ = false;
 
 public:
     Block() = default;
 
-    [[nodiscard]] std::string name() {
+    [[nodiscard]] virtual std::string name() const {
         return name_;
     }
 
-    [[nodiscard]] bool isSolid() const {
+    [[nodiscard]] virtual bool isSolid() const {
         return isSolid_;
     }
 
-    [[nodiscard]] bool isHangableOn() const {
+    [[nodiscard]] virtual bool isHangableOn() const {
         return isHangableOn_;
     }
-
-    void operator+=(Block T);
 };
 
 class Stone : public Block {
+protected:
     const std::string name_ = "stone";
     const bool isSolid_ = true;
     const bool isHangableOn_ = false;
 
 public:
     Stone() = default;
+
+    [[nodiscard]] std::string name() const final {
+        return name_;
+    }
+
+    [[nodiscard]] bool isSolid() const final {
+        return isSolid_;
+    }
+
+    [[nodiscard]] bool isHangableOn() const final {
+        return isHangableOn_;
+    }
 };
 
 class Air : public Block {
+protected:
     const std::string name_ = "air";
     const bool isSolid_ = false;
     const bool isHangableOn_ = false;
 
 public:
     Air() = default;
+
+    [[nodiscard]] std::string name() const final {
+        return name_;
+    }
+
+    [[nodiscard]] bool isSolid() const final {
+        return isSolid_;
+    }
+
+    [[nodiscard]] bool isHangableOn() const final {
+        return isHangableOn_;
+    }
 };
 
-
 class Ladder : public Block {
+protected:
     const std::string name_ = "ladder";
     const bool isSolid_ = false;
     const bool isHangableOn_ = true;
 
 public:
     Ladder() = default;
-};
 
+    [[nodiscard]] std::string name() const final {
+        return name_;
+    }
+
+    [[nodiscard]] bool isSolid() const final {
+        return isSolid_;
+    }
+
+    [[nodiscard]] bool isHangableOn() const final {
+        return isHangableOn_;
+    }
+};
 
 }  // namespace Platformer
 
