9efa047f4e37bb97c5e0fb9c13a54af7d58fa613
timustinov <112581569+timustinov@users.noreply.github.com>
2023-04-29 21:23:40 +0300

    changed model constant

--- model/include/model-constants.hpp
+++ model/include/model-constants.hpp
@@ -1,7 +1,7 @@
 namespace Platformer {
 const int BLOCK_SIZE = 1000;
-const int PLAYER_SPEED = 100;
-const int FREEFALL_ACCELERATION = 5;
+const int PLAYER_SPEED = 150;
+const int FREEFALL_ACCELERATION = 10;
 const int MAX_FIELD_SIZE = 1000;
 const float BOUNCE_COEFFICIENT = 0.5;
 }  // namespace Platformer
