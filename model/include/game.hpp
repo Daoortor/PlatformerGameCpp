3462f884da5fbd6c7929657cda748dc9a5e464fe
timustinov <112581569+timustinov@users.noreply.github.com>
2023-04-29 21:18:29 +0300

    add class Statistics

--- model/include/game.hpp
+++ model/include/game.hpp
@@ -3,6 +3,8 @@
 
 #include <memory>
 #include <vector>
+#include <chrono>
+#include <iostream>
 #include "../../tools/utilities.hpp"
 #include "block.hpp"
 #include "board.hpp"
@@ -10,7 +12,35 @@
 
 namespace Platformer {
 
+class Statistics {
+    Player *player = nullptr;
+    std::chrono::steady_clock::time_point sessionStartTime;
+    std::chrono::steady_clock::time_point startTime;
+    std::chrono::steady_clock::time_point lastRespawnTime;
+    std::chrono::steady_clock::time_point endTime;
+    bool isEnded = false;
+    int deathCount = 0;
+    double averageDeathTime = 0;
+
+public:
+    explicit Statistics() = default;
+
+    explicit Statistics(Player* player_) : player(player_) {
+        sessionStartTime = std::chrono::steady_clock::now();
+    }
+
+    void startingGame();
+    void playerDeath();
+    void endingGame();
+
+    [[nodiscard]] std::chrono::steady_clock::time_point getStartTime() const;
+    [[nodiscard]] std::chrono::steady_clock::time_point getEndTime() const;
+    [[nodiscard]] double getAverageDeathTime() const;
+    [[nodiscard]] int getDeathCount() const;
+};
+
 class Game {
+    Statistics statistics;
     Board board;
     std::unique_ptr<Player> player;
     int timer = 0;
@@ -38,10 +68,6 @@ public:
 
     Board &getBoardObject() {
         return board;
-    }
-
-    utilities::Vector getEndPos() {
-        return endPos;
     }
 
     void update();
