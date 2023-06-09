58da6c22928baef885b969e684d82693f006ca70
MaksNazarov <maksim.nazaroff.main@gmail.com>
2023-06-09 21:53:52 +0300

    latest additions

--- server/client.cpp
+++ server/client.cpp
@@ -1,8 +1,8 @@
+#include "json_file_exchange.grpc.pb.h"
+#include <fstream>
 #include <google/protobuf/util/json_util.h>
 #include <grpcpp/grpcpp.h>
-#include <fstream>
 #include <string>
-#include "json_file_exchange.grpc.pb.h"
 #include "source.hpp"
 
 using grpc::Channel;
@@ -13,54 +13,67 @@ using json_file_exchange::Act;
 using json_file_exchange::ActionReply;
 using json_file_exchange::ActionRequest;
 
+
 class LevelClient {
 private:
-    std::string level_dir_path = "../levels-client/";  // TODO: is needed?
+    std::string level_dir_path = "../levels-client/";
+    int32_t signature;
 public:
     LevelClient(const std::shared_ptr<Channel> &channel)
         : stub_(json_file_exchange::Act::NewStub(channel)) {
-    }  // TODO: is safe to make explicit?
+        signature = get_unique_key();
+    } // TODO: is safe to make explicit?
 
-    ActionReply add_level(const std::string &level_name) {
+    ActionReply add_level(const std::string& level_name) {
+        if (!file_exists(level_dir_path + level_name)) {
+            throw no_such_file(level_dir_path + level_name);
+        }
         ActionRequest request;
         request.set_action("add");
         request.set_level_name(level_name);
         json_file_exchange::LevelContent level_content;
-        google::protobuf::util::JsonStringToMessage(
-            file_content_string(level_name), &level_content
-        );
+        auto fcs_debug = file_content_string(level_dir_path + level_name);
+        std::cout << "fcs_debug: " << fcs_debug << '\n';
+        google::protobuf::util::JsonStringToMessage(fcs_debug, &level_content); // TODO: why does it fail?
+        std::cout << "add_level debug: level_content message content: " << level_content.DebugString() << '\n';
+        // TODO: learn to throw incomplete_level_file
         request.set_allocated_level_content(&level_content);
-        return sendRequest(request);  // TODO: pass by ref? What do?
-    }
+        return sendRequest(request); // TODO: pass by ref? What do?
+        // TODO: big TODO: .json fields naming & its usage in client code - CHECK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    } // TODO: rewrite level_dir_path usage
 
-    ActionReply check_level_existence(const std::string &level_name) {
+    ActionReply check_level_existence(const std::string& level_name) {
         // TODO: refactor repeated code in add, check, get, delete
         ActionRequest request;
         request.set_action("check");
         request.set_level_name(level_name);
+        request.set_signature(signature);
         return sendRequest(request);
     }
 
-    ActionReply get_level(const std::string &level_name) {
-        // TODO: refactor repeated code in add, check, get, delete// TODO:
-        // refactor repeated code in add, check, delete
+    ActionReply get_level(const std::string& level_name) {
+        // TODO: refactor repeated code in add, check, get, delete// TODO: refactor repeated code in add, check, delete
         ActionRequest request;
         request.set_action("get");
         request.set_level_name(level_name);
+        request.set_signature(signature);
         return sendRequest(request);
+        // TODO: this doesn't do much, does it? For future usage
     }
 
-    ActionReply delete_level(const std::string &level_name) {
+    ActionReply delete_level(const std::string& level_name) {
         // TODO: refactor repeated code in add, check, get, delete
         ActionRequest request;
         request.set_action("delete");
         request.set_level_name(level_name);
-        return sendRequest(request);  // TODO: look into message constructors
+        request.set_signature(signature);
+        return sendRequest(request); // TODO: look into message constructors
     }
 
-    // takes as input 3 strings; json file must be read into string using
-    // ifstream beforehand
-    ActionReply sendRequest(const ActionRequest &request) {
+    // takes as input 3 strings; json file must be read into string using ifstream beforehand
+    ActionReply sendRequest(const ActionRequest& request) {
+        std::cout << "sendRequest debug: request message content: \n==========\n"
+                  << request.DebugString() << "==========\n";
         ActionReply reply;
 
         ClientContext context;
@@ -72,8 +85,7 @@ public:
         } else {
             reply.set_result(false);
             std::cerr << "Procedure call failed\n";
-            std::cerr << status.error_code() << ": " << status.error_message()
-                      << std::endl;
+            std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
             return reply;
         }
     }
@@ -84,33 +96,62 @@ private:
 
 // example function to check workability of client class
 void RunClient() {
+
     std::string target_address("0.0.0.0:50051");
-    LevelClient client(
-        grpc::CreateChannel(target_address, grpc::InsecureChannelCredentials())
-    );
-
-    std::string action = "check";
-    std::string level_name = "t01-box-with-bladders.json";
-    std::ifstream level_file("levels/" + level_name);
-    std::string level_content_json_string;
-    level_file >> level_content_json_string;
-
-    ActionReply reply = client.add_level("t01-box-with-bladders.json");
-    if (reply.is_successful()) {
-        std::cout << "Request succeeded.\n";
-
-        std::string level_in_json;
-        google::protobuf::util::MessageToJsonString(
-            reply.level_content(), &level_in_json
-        );
-        // TODO: now I should parse reply.level_content() into file
-    } else {
-        std::cout << "Request failed\n";
+    LevelClient client(grpc::CreateChannel(target_address,
+                                           grpc::InsecureChannelCredentials())); // TODO: credentials usage
+    try {
+        // ActionReply reply = client.add_level("t01-box-with-ladder.json");
+        // ActionReply reply = client.delete_level("t01-box-with-ladder.json");
+        // ActionReply reply = client.check_level_existence("t01-box-with-ladder.json");
+        ActionReply reply = client.get_level("test.json");
+        if (reply.is_successful()) {
+            std::cout << "Request succeeded.\n";
+            std::cout << "Request result: result=" << reply.result() << ", is_successful=" <<reply.is_successful() << '\n';
+            std::string level_in_json;
+            google::protobuf::util::MessageToJsonString(reply.level_content(), &level_in_json);
+            std::cout << level_in_json;
+            // TODO: TODO
+        } else {
+            std::cout << "Request failed\n";
+        }
+    } catch (const file_handling_exception &file_exception) {
+        std::cerr << file_exception.what();
+        return;
+    } catch (const std::runtime_error & error) {
+        std::cerr << error.what();
+        return;
     }
-}  // TODO: clear this function
+
+
+    json_file_exchange::LevelContent level_content;
+
+    level_content.set_levelname(std::string("Test"));
+    level_content.set_width(100);
+    level_content.set_height(100);
+
+    json_file_exchange::LevelContent_BlockRow block_row;
+    std::string block_data[] = {"air", "stone", "air"};
+    block_row.ParseFromArray(block_data, 3);
+    level_content.mutable_blockmap()->Add()->CopyFrom(block_row); // AddAllocated(&block_row);
+
+    level_content.mutable_playerstartpos()->Add(50);//set_x(50);
+    level_content.mutable_playerstartpos()->Add(50);
+    level_content.mutable_endpos()->Add(60);
+    level_content.mutable_endpos()->Add(60);
+
+    std::string level_in_json = file_content_string("../levels-client/test.json");
+    google::protobuf::util::JsonStringToMessage(level_in_json, &level_content);
+    google::protobuf::util::MessageToJsonString(level_content, &level_in_json); // TODO: does it work?
+    google::protobuf::util::JsonStringToMessage(level_in_json, &level_content);
+    google::protobuf::util::MessageToJsonString(level_content, &level_in_json); // TODO: does it work?
+    // add_or_replace_json_file("../levels-client/test.json", level_in_json);
+    std::cout << level_in_json;
+
+
+} // TODO: clear this function
 
 int main(int argc, char *argv[]) {
     RunClient();
-
     return 0;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -1,8 +1,8 @@
-#include <grpcpp/grpcpp.h>
+#include "json_file_exchange.grpc.pb.h"
 #include <filesystem>
 #include <fstream>
+#include <grpcpp/grpcpp.h>
 #include <string>
-#include "json_file_exchange.grpc.pb.h"
 #include "source.hpp"
 
 using grpc::Server;
@@ -14,93 +14,88 @@ using json_file_exchange::Act;
 using json_file_exchange::ActionReply;
 using json_file_exchange::ActionRequest;
 
-class file_handling_exception : public std::runtime_error {
-public:
-    explicit file_handling_exception(const std::string &what_arg)
-        : std::runtime_error("Error: " + what_arg){};
-};
-
-class no_such_file : public file_handling_exception {
-public:
-    explicit no_such_file(const std::string &file_name)
-        : file_handling_exception("no such file: " + file_name){};
-};
-
-class incomplete_level_file : public file_handling_exception {
-public:
-    explicit incomplete_level_file(const std::string &level_name)
-        : file_handling_exception(
-              "missing crucial info for level file creation/overwrite: " +
-              level_name
-          ){};
-};  // TODO: is needed at all?
-
 // Server Implementation
-class ReverseServiceImplementation final : public Act::Service {
+class LevelManagementServer final : public Act::Service {
 private:
     std::string level_dir_path = "../levels-server/";
-
 public:
-    Status sendRequest(
-        ServerContext *context,
-        const ActionRequest *request,
-        ActionReply *reply
-    ) override {
-        auto &action = request->action();
+    static void handleRequestAdd(const json_file_exchange::LevelContent& level_content,
+                                 const std::string & level_file_path) {
+        std::string level_in_json;
+        google::protobuf::util::MessageToJsonString(level_content, &level_in_json); // TODO: does it work?
+        std::cout << "\nAdding a level " << level_file_path
+                  << " with content:\n-----\n" << level_in_json
+                  << "-----\n";
+        add_or_replace_json_file(level_file_path, level_in_json);
+        // TODO: confirmation checkbox "Such file already exists. Do you wish to overwrite it?"
+    }
+    static void handleRequestDelete(const std::string & level_file_path) {
+        if (!file_exists(level_file_path)) throw no_such_file(level_file_path);
+        std::cout << "\nDeleting a level " << level_file_path << '\n';
+        delete_file(level_file_path);
+    }
+    static void handleRequestGet(ActionReply *reply, const std::string & level_file_path) {
+        if (!file_exists(level_file_path)) throw no_such_file(level_file_path);
+        std::cout << "\nGetting a level " << level_file_path
+                  << " with content:\n-----\n" << file_content_string(level_file_path)
+                  << "\n-----\n";
+        json_file_exchange::LevelContent requested_level_content; // TODO: use sendRequest' own level_content, then swap?
+        google::protobuf::util::JsonStringToMessage(file_content_string(level_file_path), &requested_level_content);
+        std::cout << "LevelContent looks like:\n++++++++++\n" << requested_level_content.DebugString() << "\n++++++++++\n";
+        /*
+        std::string json_debug_string;
+        google::protobuf::util::MessageToJsonString(requested_level_content, &json_debug_string);
+        std::cout << "Level string: " << json_debug_string;
+         */
+        reply->mutable_level_content()->Swap(&requested_level_content);
+        reply->set_result(true);
+    }
+    static void handleRequestCheck(ActionReply *reply, const std::string & level_file_path) {
+        std::cout << "\nChecking existence of level " << level_file_path << '\n';
+        std::cout << "Check result: " << file_exists(level_file_path) << '\n';
+        reply->set_result(file_exists(level_file_path));
+    }
+    Status sendRequest(ServerContext *context, const ActionRequest *request,
+                       ActionReply *reply) override {
+
+        auto & action = request->action();
         std::string level_file_path = level_dir_path + request->level_name();
-        auto &level_content = request->level_content();
+        auto & level_content = request->level_content(); // TODO: is it correct type?
 
-        std::cout << "Requested action: " << action << '\n';
-        std::cout << "Level name: " << level_file_path << '\n';
-
+        std::cout << "===========\nNew request:\n"
+                  << "Requested action: " << action << '\n'
+                  << "Level name: " << level_file_path << '\n'
+                  << "Request signature: " << request->signature() << '\n';
         try {
             if (action == "add") {
-                std::string level_in_json;
-                google::protobuf::util::MessageToJsonString(
-                    level_content, &level_in_json
-                );
-                add_or_replace_json_file(level_file_path, level_in_json);
-                // TODO: confirmation checkbox "Such file already exists. Do you
-                // wish to overwrite it?"
+                handleRequestAdd(level_content, level_file_path);
             } else if (action == "delete") {
-                if (!file_exists(level_file_path))
-                    throw no_such_file(level_file_path);
-                delete_file(level_file_path);
+                handleRequestDelete(level_file_path);
             } else if (action == "get") {
-                // TODO: add actions: get string by ifstream via
-                // request.level_name(),
-                //  save to reply.level_content() by JsonStringToMessage()
-                if (!file_exists(level_file_path))
-                    throw no_such_file(level_file_path);
-                json_file_exchange::LevelContent requested_level_content;
-                google::protobuf::util::JsonStringToMessage(
-                    file_content_string(level_file_path),
-                    &requested_level_content
-                );
-                reply->set_allocated_level_content(&requested_level_content);
-                reply->set_result(true);
+                handleRequestGet(reply, level_file_path);
             } else if (action == "check") {
-                reply->set_result(file_exists(level_file_path));
+                handleRequestCheck(reply, level_file_path);
             }
             reply->set_is_successful(true);
         } catch (const file_handling_exception &file_exception) {
             std::cerr << file_exception.what();
             reply->set_is_successful(false);
             return Status::CANCELLED;
-        }
+        } catch (const std::runtime_error & error) {
+            std::cerr << error.what();
+            return Status::CANCELLED;
+        } // TODO: expand on error handling
         return Status::OK;
     }
-
-    // TODO: move different actions to different private methods
 };
 
 void RunServer() {
     std::string server_address("0.0.0.0:50051");
-    ReverseServiceImplementation service;
+    LevelManagementServer service;
 
     ServerBuilder builder;
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    // TODO: check gRPC version for line below; in 1.54 it should take 1 arg;
+    // TODO: check gRPC version for line above; in 1.54 it should take 1 arg;
     //  installed version is 1.54, but this function requests 2 args
     builder.RegisterService(&service);
 
